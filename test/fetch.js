4aac27925ce6abcda054521be2cc428f062bebc7
Sam Verschueren <sam.verschueren@gmail.com>
2016-07-13 20:54:17 +0200

    fix `fetch` cache key

--- test/fetch.js
+++ test/fetch.js
@@ -13,6 +13,7 @@ test.before(() => {
 	nock(URL).get('/no-cache').reply(200, {foo: 'bar'});
 	nock(URL).get('/cache').once().reply(200, {hello: 'world'});
 	nock(URL).get('/cache').twice().reply(200, {hello: 'world!'});
+	nock(URL).get('/cache-key?unicorn=rainbow').reply(200, {unicorn: 'rainbow'});
 });
 
 test('no cache', async t => {
@@ -27,4 +28,9 @@ test('cache', async t => {
 	await delay(5000);
 
 	t.deepEqual(await m.fetch(`${URL}/cache`, {maxAge: 5000}), {hello: 'world!'});
+});
+
+test('cache key', async t => {
+	t.deepEqual(await m.fetch(`${URL}/cache-key`, {query: {unicorn: 'rainbow'}, maxAge: 5000}), {unicorn: 'rainbow'});
+	t.truthy(m.cache.store['http://foo.bar/cache-key{"json":true,"query":{"unicorn":"rainbow"},"maxAge":5000}']);
 });
