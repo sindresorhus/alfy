73f493cb448ae9ab4f4e4beaa7675407872dd0cb
Jacob Hilker <hilker.j@gmail.com>
2016-10-13 11:22:29 -0400

    Clarify error message for missing `$alfred_workflow_cache`
    The `$alfred_workflow_cache` env variable is blank if your new workflow is missing a `Bundle Id`.
    In this case, the error message is fairly unintuitive to determine what exactly is wrong, hopefully this will prevent anyone else from ever getting caught up like I did :).

--- run-node.sh
+++ run-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z "$alfred_workflow_cache" ]]; then
-	echo "This script must be called from Alfred, \$alfred_workflow_cache is missing"
+	echo "This script must be called from Alfred, \$alfred_workflow_cache is missing. Make sure a bundleId is set."
 	exit 1
 fi
 
