44a19b41081375540f7c1ca6408a5482a8dd7a88
Sam Verschueren <sam.verschueren@gmail.com>
2016-10-28 18:12:38 +0200

    :arrow_up: cache-conf@0.3.0 - fixes #34 (#37)

--- test/_utils.js
+++ test/_utils.js
@@ -3,10 +3,12 @@
 const path = require('path');
 const tempfile = require('tempfile');
 
-exports.alfy = () => {
+exports.alfy = options => {
+	options = options || {};
+
 	delete require.cache[path.resolve(__dirname, '../index.js')];
 
-	process.env.alfred_workflow_cache = tempfile();
-	process.env.alfred_workflow_version = '1.0.0';
+	process.env.alfred_workflow_cache = options.cache || tempfile();
+	process.env.alfred_workflow_version = options.version || '1.0.0';
 	return require('..');
 };
