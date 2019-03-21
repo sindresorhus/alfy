d3ab474c326015786146813dd64b94790d2a4add
Federico Brigante <github@bfred.it>
2019-03-22 00:26:12 +0800

    Use long flag

--- run-node.sh
+++ run-node.sh
@@ -43,7 +43,7 @@ if ! has_node; then
 fi
 
 if has_node; then
-	node -r esm "$@"
+	node --require esm "$@"
 else
 	echo $'{"items":[{"title": "Couldn\'t find the `node` binary", "subtitle": "Symlink it to `/usr/local/bin`"}]}'
 fi
